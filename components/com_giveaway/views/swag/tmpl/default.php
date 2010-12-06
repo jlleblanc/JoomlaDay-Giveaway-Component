f524ae6f30532434918661246f5ba6d447e40dc3
Joe LeBlanc <contact@jlleblanc.com>
2010-12-05 19:19:02 -0500

    Removed extraneous views and models from frontend

--- components/com_giveaway/views/swag/tmpl/default.php
+++
@@ -1,9 +0,0 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<div id="swag">
-	<dl>
-		<dt>Attendee_id</dt>
-			<dd><?php echo $this->row->attendee_id ?></dd>
-		<dt>Name</dt>
-			<dd><?php echo $this->row->name ?></dd>
-	</dl>
-</div>
